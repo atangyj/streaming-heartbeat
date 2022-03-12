8848223f23b1ab893ea7804f95c4823d50c861c6
Yong Jheng Tang <tyj@yongs-mbp.home>
2022-03-12 21:28:10 +0000

    delete unused files after refactor

--- src/utils/logger.ts
+++
@@ -1,9 +0,0 @@
-import winston from "winston";
-
-export const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "info.log", level: "info" }),
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-  ],
-});
